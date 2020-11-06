3eaf313de75c0b26670f0b4fa7e2bf8743c22102
Nir Moav <nirmoav@fb.com>
2020-11-06 16:10:24 +0200

    wip - sending push notif to client works with manual get to api/trigger

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
+const fs = require('fs')
+const VPAID_KEYS = {
+    publicKey: 'BNjlC0ub2WVA5hGekdsX7bRJGQ_LpoVAujoJYA1cwDouBriHZBxQW_4Q_sLE7vSaoLQcbWqZNOhBQ9YBXQoR3YY',
+    privateKey: fs.readFileSync('./vapid-pkey.txt', {encoding: 'UTF8'})
+}
+
+// console.log("pkey = ", VPAID_KEYS.private)
+
+const webpush = require('web-push');
+webpush.setVapidDetails(
+  'mailto:getnirm@google.com',
+  VPAID_KEYS.publicKey,
+  VPAID_KEYS.privateKey
+);
+
+
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser')
@@ -8,17 +24,26 @@ var jsonParser = bodyParser.json()
 
 var pushSub = null
 
+function responseOK(res) {
+    res.setHeader('Content-Type', 'application/json')
+    res.send(JSON.stringify({'status': 'OK'}))
+}
+
 
 app.get('/ping', function (req, res) {
     console.log("PING REQUEST!")
     return res.send('pong');
 });
 
+app.get('/api/trigger-push', function(req, res) {
+    webpush.sendNotification(pushSub.data, JSON.stringify({title: "hello from server :)"}))
+    responseOK(res)
+})
+
 app.post('/api/save-push-sub', jsonParser, function(req, res) {
     const payload = req.body
     pushSub = payload//JSON.parse(payload)
-    res.setHeader('Content-Type', 'application/json')
-    res.send(JSON.stringify({'status': 'OK'}))
+    responseOK(res)
     console.log('STORED PUSH SUBSCRIPTION', pushSub)
 })
 
